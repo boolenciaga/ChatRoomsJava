d6a2cb68f29018487eadd8614631201f9d35535a
majinmilad <49212431+majinmilad@users.noreply.github.com>
2020-03-20 22:51:59 -0700

    make Server class to handle multiple client connections
    and ClientConnection nested class to hold client specific socket

--- Server Backend/Server.java
+++ Server Backend/Server.java
@@ -8,28 +8,98 @@ import java.net.Socket;
 
 public class Server
 {
-    public static void main(String args[])
+    //driver for testing class
+    public static void main(String[] args)
     {
+        new Server();
+    }
+
+
+    private int portNumber = 6000;
+    private int numOfClients = 0;
+
+
+    public Server()
+    {
+        System.out.println("Server turned on\n");
+
         try
         {
-            ServerSocket ss = new ServerSocket(5000);
+            //establish server on a port
+            ServerSocket ss = new ServerSocket(portNumber);
 
-            Socket socket = ss.accept();
+            //server open for connections while some condition
+            while(true)
+            {
+                Socket socket = ss.accept();
 
-            System.out.println("\nServer-Client connection established...\n");
+                ++numOfClients;
 
-            InetAddress clientAddress = socket.getInetAddress();
-            System.out.println("(Connected to " + clientAddress.getHostName() + ")\n\n");
+                displayConnectionStatus(socket);
 
-            BufferedReader serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String incomingString = serverInput.readLine();
-            System.out.println("Server RECEIVED : "+incomingString+"\n");
-
-            PrintWriter serverOutput = new PrintWriter(socket.getOutputStream(), true);
-            serverOutput.println(incomingString+" {sent back from server}\n\n");
+                //Form a new thread for client connection
+                new Thread(new ClientConnection(socket, numOfClients)).start();
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    private static class ClientConnection implements Runnable
+    {
+        private Socket serverToClientSocket;
+        private int clientNumber;
+
+        public ClientConnection(Socket socket, int clientNumber)
+        {
+            serverToClientSocket = socket;
+            this.clientNumber = clientNumber;
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                //Establish IO streams
+                BufferedReader inputFromClient = new BufferedReader(new InputStreamReader(serverToClientSocket.getInputStream()));
+
+                PrintWriter outputToClient = new PrintWriter(serverToClientSocket.getOutputStream(), true);
+
+                //Serve the client
+                while(true)
+                {
+                    String incomingString = inputFromClient.readLine();
+                    System.out.println("Server RECEIVED : " + incomingString + " {from client #"+clientNumber+"}\n");
+
+                    outputToClient.println(incomingString + " {sent back from server}");
+                }
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /*********************************************************************************/
+
+
+    private void displayConnectionStatus(Socket socket)
+    {
+        if(socket.isConnected())
+        {
+            InetAddress clientAddress = socket.getInetAddress();
+
+            System.out.println("Server-Client connection established...");
+            System.out.println("(Connected to " + clientAddress.getHostName() + " [IP: " + clientAddress.getHostAddress() + "])\n\n");
+        }
+        else
+        {
+            System.out.println("-- SOCKET NOT CONNECTED --\n");
+        }
+
     }
 }
